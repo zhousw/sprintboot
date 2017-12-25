27094a9949b9e8a1377f6611d92f059ff7154b18
zhousw <405884954@qq.com>
2017-12-25 22:16:20 +0800

    zhousw2

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.mapper")
-// 告诉mapper所在的包名zhousw1
+// 告诉mapper所在的包名zhousw2
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
